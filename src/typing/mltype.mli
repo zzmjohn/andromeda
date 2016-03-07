908d0dc2e5486d9c012d2fad78b12e558ae67af8
Andrej Bauer <Andrej.Bauer@andrej.com>
2016-03-07 16:32:47 +0100

    add forgotten files

---
+++ src/typing/mltype.mli
@@ -0,0 +1,11 @@
+(** The AML types. *)
+
+type bound_info =
+  | BoundVal
+  | BoundConst of Name.constant
+  | BoundData of Name.data * int
+  | BoundOp of Name.operation * int
+  | BoundSig of Name.signature
+  | BoundDyn of Store.Dyn.key
+
+type ctx = (Name.ident * bound_info) list
